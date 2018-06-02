0d015dde56683a888775139825ac2f0859068768
Bar Perlman <barper@post.bgu.ac.il>
2018-06-02 18:39:23 +0300

    board with characters

--- src/ViewComponents/BoardPanel.java
+++ src/ViewComponents/BoardPanel.java
@@ -9,51 +9,34 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel {
-	String lvl1BlockPath = "images/block25.png";
-	ImageIcon blockImage;
+	String _lvl1BlockPath = "images/block25.png";
+	ImageIcon _blockImage;
+	ImageIcon _pillImage;
+	ImageIcon _energyImage;
+	ImageIcon _pineappleImage;
+	ImageIcon _appleImage;
+	ImageIcon _strawberryImage;
+	ImageIcon _emptyImage;
+	
 	// gameTools:
 	final static int EMPTY = 0, BLOCK = 1, PACMAN = 2, GHOST1 = 3, GHOST2 = 4, GHOST3 = 5, PILL = 6, ENERGY = 7,
 			PINEAPPLE = 8, APPLE = 9, STRAWBERRY = 10;
 
-	// receivedBoard initialized to the level1 board
+	
 	// the controller updates later the board appearance
-	private int[][] receivedBoard;/*
-									 * ={ {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,0,0,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,0,1,1,0,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,0,1,0,0,1,1,0,1,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,0,1,0,0,1,1,0,1,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,0,1,0,0,1,1,0,1,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,0,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,1,0,0,1,1,1,1,1,0,1,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,1,0,1,1,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,1,1,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,0,0,1,0,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1},
-									 * {1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
-									 * {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1} };
-									 */
+	private int[][] receivedBoard;
 
 	public BoardPanel() {
-		blockImage = new ImageIcon(lvl1BlockPath);
+		_blockImage = new ImageIcon(_lvl1BlockPath);
+		_pillImage = new ImageIcon("images/pill.png");
+		_energyImage = new ImageIcon("images/energy.png");
+		_pineappleImage = new ImageIcon("images/pineapple.png");
+		_appleImage = new ImageIcon("images/apple.png");
+		_strawberryImage = new ImageIcon("images/strawberry.png");
+		_emptyImage=new ImageIcon("images/empty.png");
+		
+		
+		
 	}
 
 	public void paint(Graphics g) {
@@ -65,7 +48,25 @@ public class BoardPanel extends JPanel {
 			for (int j = 0; j < receivedBoard.length; j++) {
 				if (receivedBoard[i][j] == BLOCK) {
 					// g.fillRect(i*25, j*25, 25, 25);
-					blockImage.paintIcon(this, g2, i * 25, j * 25);
+					_blockImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==PILL){
+					_pillImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==ENERGY){
+					_energyImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==PINEAPPLE){
+					_pineappleImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==APPLE){
+					_appleImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==STRAWBERRY){
+					_strawberryImage.paintIcon(this, g2, i * 25, j * 25);
+				}
+				else if(receivedBoard[i][j]==EMPTY){
+					_emptyImage.paintIcon(this, g2, i * 25, j * 25);
 				}
 			}
 		}
